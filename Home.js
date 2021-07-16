ed5d1403762ed924103b389c438a704ec078c274
AAyres04 <a.ayres01@ufromail.cl>
2021-07-16 18:54:20 -0400

    navegacion a opciones implementada

--- Home.js
+++ Home.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function Home() {
+export default function Home({navigation}) {
     const [temperaturaActual, setTemperaturaActual] = useState(0);
+    const [gasCO2Actual, setGasCO2Actual] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(()=>{
             tickTemp();
+            tickCO2lecture();
         }, 1000);
         return () => clearInterval(interval);
     }, []);
@@ -15,8 +17,20 @@ export default function Home() {
         var newTemp = getTemperaturaActual()
         setTemperaturaActual(newTemp);
     }
+    const tickCO2lecture = () => {
+        var newCO2lecture = getGasCO2Actual();
+        setGasCO2Actual(newCO2lecture);
+    }
 
     const getTemperaturaActual = () => {
+        //to-do: comunicar la aplicacion con tinkercad usando mqtt(?)
+        const min = 1;
+        const max = 100;
+        var rand = min + Math.random() * (max - min);
+        return rand;
+    }
+
+    const getGasCO2Actual = () => {
         //to-do: comunicar la aplicacion con tinkercad usando mqtt(?)
         const min = 1;
         const max = 100;
@@ -30,6 +44,16 @@ export default function Home() {
             <View style={styles.boxTemp}>
                 <Text>{temperaturaActual}</Text>
             </View>
+            <Text>CO2 actual</Text>
+            <View style={styles.boxTemp}>
+                <Text>{gasCO2Actual}</Text>
+            </View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={()=> navigation.navigate('Options')}
+            >
+                <Text>Opciones</Text>
+            </TouchableOpacity>
             <StatusBar style="auto" />
         </View>
     );
@@ -43,11 +67,20 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     boxTemp:{
-        flex: 1,
+        flex: 2,
         backgroundColor:'#efefef',
         borderStyle:'solid',
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: 'black',
+        marginBottom: 10,
+        padding: 10
     },
-    
+    button:{
+        backgroundColor:'#0fa30f',
+        borderStyle:'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+        padding: 10
+    }
 });
