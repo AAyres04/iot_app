8f03969a84e63fa0516c8a5f596a19ec72f6b459
amendezllaupe <amendezllaupe@gmail.com>
2021-07-16 21:13:32 -0400

    Se actualiza Options.js

--- Options.js
+++ Options.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import NumericInput from 'react-native-numeric-input'
 
 export default function Home() {
+    
+    const [temperaturaIdeal, setTemperaturaIdeal] = useState(0);
+    const [temperaturaTemp, setTemperaturaTemp] = useState(0);
+    const [dioxidoTemp, setDioxidoTemp] = useState(0);
+    const [dioxidoIdeal, setDioxidoIdeal] = useState(0);
+
     return(
-        <View>
-            <Text>implement me</Text>
+        <View style={styles.container}>
+            <Text>Ingresa la temperatura ideal</Text>
+            <View style={styles.row}>
+                <TextInput 
+                    keyboardType='numeric'
+                    style={styles.input}
+                    placeholder='ej. 40°C'
+                    onChangeText={(val)=> setTemperaturaTemp(val)}/>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={()=> setTemperaturaIdeal(temperaturaTemp)}>
+                        <Text style={styles.textButton}>Actualizar</Text>
+                    </TouchableOpacity>
+            </View>
+            
+
+            <Text>Ingresa el CO2 máximo</Text>
+            <View style={styles.row}>
+                <TextInput 
+                    keyboardType='numeric'
+                    style={styles.input}
+                    placeholder='ej. 40ppm'
+                    onChangeText={(val)=> setDioxidoTemp(val)}/>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={()=> setDioxidoIdeal(dioxidoTemp)}>
+                        <Text style={styles.textButton}>Actualizar</Text>
+                    </TouchableOpacity>
+            </View>
+
+            <Text>Temperatura Ideal: {temperaturaIdeal}°C, CO2 Máximo: {dioxidoIdeal}ppm</Text>
         </View>
     );
 }
@@ -24,5 +60,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'black'
     },
-    
+    input:{
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200,
+    },
+    button:{
+        backgroundColor:'#0fa30f',
+        borderStyle:'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+        padding: 10,
+    },
+    textButton:{
+        color: 'white',
+    },
+    row:{
+        flexDirection: 'row',
+    }
 });
