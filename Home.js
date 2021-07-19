48448d920446e8dcaa56a3189f61ae3c67594a9b
AAyres04 <a.ayres01@ufromail.cl>
2021-07-18 20:03:30 -0400

    Se extiende la duracion de envio auxiliar a 2 segundos

--- Home.js
+++ Home.js
@@ -14,14 +14,16 @@ const myStorage = {
   };
 
 export default function Home({navigation}) {
+    //Variables de estado, para cambiar dinamicamente los valores en la aplicacion.
     const [temperaturaActual, setTemperaturaActual] = useState(0);
     const [gasCO2Actual, setGasCO2Actual] = useState(0);
     const [mensaje, setMensaje] = useState({
         temperatura: '',
         gasCO2Actual: ''
     });
+
+    //Creacion de cliente
     const myUri = "ws://broker.emqx.io:8083/mqtt"
-    
     const clientId = () => {
         const min = 1;
         const max = 1000;
@@ -30,9 +32,9 @@ export default function Home({navigation}) {
         return randString;
     }
     const topic = "ejemplomqtt/mbaaaam";
-
     const client = new Client({ uri: myUri, clientId: clientId(), storage: myStorage});
 
+    //Eventos de cliente
     client.on('connectionLost', (responseObject) => {
     if (responseObject.errorCode !== 0) {
         console.log(responseObject.errorMessage);
@@ -41,9 +43,9 @@ export default function Home({navigation}) {
     client.on('messageReceived', (message) => {
         //Evento que maneja el mensaje recibido.
         setMensaje(parseAndSetMqttData(message.payloadString));
-        //parseAndSetMqttData(message.payloadString)
     });
 
+    //Despues de que el componente 'Home' se monta o renderiza, se ejecuta lo siguiente:
     useEffect(() => {
         client.connect()
         .then(() => {
@@ -61,31 +63,7 @@ export default function Home({navigation}) {
         return () => clearInterval(interval);
     }, []);
 
-    const tickMqttAuxSendData = () => {
-        //Funcion para enviar los datos generados al topico.
-        //Esta funcion es para simular el envio de datos en la aplicacion, debido a que no se pueden enviar datos desde tinkercad.
-        //StringMessage es una variable auxiliar. En esta se construye el mensaje que la simulacion debiese enviar
-        const stringMessage = 'temperatura:' + getTemperaturaActual() + "\n" + 'ppm:' + getGasCO2Actual()
-        const message = new Message(stringMessage);
-        message.destinationName = topic
-        client.send(message);
-    }
-
-    const getTemperaturaActual = () => {
-        //Como no se puede conectar a la simulacion tinkercad, utilizamos un valor random.
-        const min = 15;
-        const max = 30;
-        var rand = Math.floor(min + Math.random() * (max - min)) + 1;
-        return rand;
-    }
-    const getGasCO2Actual = () => {
-        //Como no se puede conectar a la simulacion tinkercad, utilizamos un valor random.
-        const min = 200;
-        const max = 800;
-        var rand = Math.floor(min + Math.random() * (max - min)) + 1;
-        return rand;
-    }
-
+    //Funcion que parsea los datos recibidos del mensaje mqtt.
     const parseAndSetMqttData = (input) => {
         var partes = input.split('\n');
         var partesTemperatura = partes[0].split(':');
@@ -100,6 +78,34 @@ export default function Home({navigation}) {
             gasCO2Actual:auxGasCO2Actual
         }
         return newMsg;
+    }
+
+    //Funcion auxiliar para enviar los datos generados al topico.
+    const tickMqttAuxSendData = () => {
+        //Esta funcion es para simular el envio de datos en la aplicacion, debido a que no se pueden enviar datos desde tinkercad.
+        //StringMessage es una variable auxiliar. En esta se construye el mensaje que la simulacion debiese enviar
+        const stringMessage = 'temperatura:' + getTemperaturaActual() + "\n" + 'ppm:' + getGasCO2Actual()
+        const message = new Message(stringMessage);
+        message.destinationName = topic
+        client.send(message);
+    }
+
+    //Funcion auxiliar para generar el valor temperatura.
+    const getTemperaturaActual = () => {
+        //Como no se puede conectar a la simulacion tinkercad, utilizamos un valor random.
+        const min = 15;
+        const max = 30;
+        var rand = Math.floor(min + Math.random() * (max - min)) + 1;
+        return rand;
+    }
+
+    //Funcion auxiliar para generar el valor CO2.
+    const getGasCO2Actual = () => {
+        //Como no se puede conectar a la simulacion tinkercad, utilizamos un valor random.
+        const min = 200;
+        const max = 800;
+        var rand = Math.floor(min + Math.random() * (max - min)) + 1;
+        return rand;
     }
 
     return (
